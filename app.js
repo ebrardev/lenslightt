57abee40b9368006cdb368f57318608365589a3a
ebrardev <16155052@mersin.edu.tr>
2024-01-02 11:43:49 +0300

    register

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 import connectDB from "./db.js"
 import pageRoute from "./routes/pageRoute.js"
 import photoRoute from "./routes/photoRoute.js"
-
+import userRoute from "./routes/userRoute.js"
 dotenv.config()
 
 connectDB()
@@ -17,9 +17,11 @@ app.set("view engine", "ejs")
 // static files middleware
 app.use(express.static("public"))
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 // routes
 app.use("/", pageRoute)
 app.use("/photos", photoRoute)
+app.use("/users", userRoute)
 
 
 app.listen(port, () => {
